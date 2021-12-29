4eb8bb0422c659b437ee708cbf44de60438b1ecc
KJH <daeding7@naver.com>
2021-12-29 20:23:14 +0900

    서버유틸 글래트 기능 추가 -> 로그인을 실제로 실행시켜주는 기능을 추가

--- app/src/main/java/com/kim/colosseum_kim/utilis/Serverutil.kt
+++ app/src/main/java/com/kim/colosseum_kim/utilis/Serverutil.kt
@@ -1,5 +1,9 @@
 package com.kim.colosseum_kim.utilis
 
+import okhttp3.FormBody
+import okhttp3.OkHttpClient
+import okhttp3.Request
+
 class Serverutil {
 //    어떤 내용? = 서버 연결 전담.
 
@@ -10,7 +14,36 @@ companion object{
 
 //    로그인 하는 기능
 
-    fun postRequestLogin(){
+    fun postRequestLogin(email : String, pw : String){
+
+//        서버에 입력받은 email, pw 전달 => 로그인 기능 POST/user => 요청 (Request) 실행
+//        라이브러리(okHttp) 활용해 보자
+
+
+//        http://54.180.52.26/user + POST + 파라미터들 첨부.
+//        호스트 주소 + 기능주소 결합
+        val urlString = "${BASE_URL}/user"
+
+//        POST 방식 => 파라미터를 폼데이터에 담아주자
+
+        val formData = FormBody.Builder()
+            .add("email", email)
+            .add("password", pw)
+            .build()
+
+//        어디로 어떻게 어떤 데이터를 들고 가는지를 모두 종합해둔 Request 변수 생성
+        val request = Request.Builder()
+            .url(urlString)
+            .post(formData)
+            .build()
+
+//        클라이언트로써의 동작 : Request 요청 실행. =>okHttp라이브러리 지원.
+
+        val client = OkHttpClient()
+
+//        실제로 서버에 요청 날리기.
+        client.newCall(request)
+
 
 
     }
